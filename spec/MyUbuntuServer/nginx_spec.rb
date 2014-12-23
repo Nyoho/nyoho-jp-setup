9e63314292c19488bd02160b68bebbc657727a2a
北䑓如法 <algebraicallyClosedField@gmail.com>
2014-12-23 23:23:49 +0900

    improve nginx_spec

--- spec/MyUbuntuServer/nginx_spec.rb
+++ spec/MyUbuntuServer/nginx_spec.rb
@@ -10,5 +10,25 @@ describe port(80) do
 end
 
 # nginx で ltsv log になっている
+describe file('/etc/nginx/nginx.conf') do
+  its(:content) { should match %r|access_log /var/log/nginx/access.log ltsv;| }
+end
+
 # nginx で nyoho.jp があって root が /var/www になっている
-# /var/www の owner nyoho になっている
+describe file('/etc/nginx/sites-available/nyoho.jp') do
+  it { should be_file }
+end
+describe file('/etc/nginx/sites-enabled/nyoho.jp') do
+  it { should be_symlink }
+end
+describe file('/etc/nginx/sites-available/nyoho.jp') do
+  its(:content) { should match %r|root /var/www/nyoho.jp;| }
+end
+
+# /var/www/nyoho.jp が存在して owner が nyoho になっている
+describe file('/var/www/nyoho.jp') do
+  it { should be_directory }
+end
+describe command('ls -ld /var/www/nyoho.jp') do
+  its(:stdout) { should match /2 nyoho/ }
+end
